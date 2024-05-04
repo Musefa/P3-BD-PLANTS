6b7c09566b397cce4e580f647aec3f251e151059
Eric García Castro <ericgar2003@gmail.com>
2024-05-04 16:46:23 +0200

    CAMBIO TRIGGERS

--- data_base_set_up.sql
+++ data_base_set_up.sql
@@ -1,7 +1,7 @@
 -- BORRADO Y CREACION DE LA BASE DE DATOS
 DROP SCHEMA IF EXISTS PLANTS;
 CREATE SCHEMA PLANTS;
-
+USE PLANTS;
 -- DEFINICION DE TABLAS DE LA BASE DE DATOS
 CREATE TABLE Plantes
 (
@@ -16,9 +16,37 @@ CREATE TABLE Metodes_Reproduccio
 
 CREATE TABLE Reproduccions
 (
-    nom_planta VARCHAR(50),
+    nom_planta VARCHAR(50) NOT NULL,
+    nom_metode VARCHAR(50) NOT NULL,
+    grau_exit SMALLINT,
     FOREIGN KEY (nom_planta) REFERENCES Plantes(nom_popular),
-    nom_metode VARCHAR(50),
-    FOREIGN KEY (nom_metode) REFERENCES Metodes_Reproduccio(nom_popular),
-    grau_exit SMALLINT
+    FOREIGN KEY (nom_metode) REFERENCES Metodes_Reproduccio(nom_popular)
 );
+
+-- INSERCIONES DEFAULT
+INSERT INTO Plantes (nom_popular, nom_llati) VALUES ('default_nom_popular', 'default_nom_llati');
+INSERT INTO Metodes_Reproduccio (nom_popular) VALUES ('default_metode_reproduccio');
+INSERT INTO Reproduccions (nom_planta, nom_metode, grau_exit) VALUES ('default_nom_popular', 'default_metode_reproduccio', 0);
+
+-- DEFINICION DE TRIGGERS
+DELIMITER //
+
+CREATE TRIGGER after_plantes_insert
+AFTER INSERT ON Plantes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Reproduccions (nom_planta, nom_metode, grau_exit)
+    VALUES (NEW.nom_popular, 'default_metode_reproduccio', NULL);
+END//
+
+CREATE TRIGGER after_metodes_reproduccio_insert
+AFTER INSERT ON Metodes_Reproduccio
+FOR EACH ROW
+BEGIN
+    INSERT INTO Reproduccions (nom_planta, nom_metode, grau_exit)
+    VALUES ('default_nom_popular', NEW.nom_popular, NULL);
+END//
+
+DELIMITER ;
+
+
