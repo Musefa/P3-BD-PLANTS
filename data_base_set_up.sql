20ca761d4fcb9e4bd54dd68857d411219ecdee4b
Ivan <imolimort@gmail.com>
2024-05-07 16:30:48 +0200

    Creacio feta, insercions fetes menys exemplars

--- data_base_set_up.sql
+++ data_base_set_up.sql
@@ -7,32 +7,32 @@ USE PLANTS;
 CREATE TABLE firmes_comercials
 (
     nom CHAR(50),
-    PRIMARY KEY (nom)
-);
+    CONSTRAINT pk_firmes_comercials PRIMARY KEY (nom)
+) ENGINE = InnoDB;
 
 CREATE TABLE adobs
 (
     nom CHAR(50),
     tipus CHAR(50),
     nom_firma_comercial CHAR(50) NOT NULL,
-    CONSTRAINT pk_adobs PRIMARY KEY (nom) NOT DEFERRABLE,
-    FOREIGN KEY (nom_firma_comercial) REFERENCES firmes_comercials(nom) ON DELETE CASCADE ON UPDATE CASCADE
-);
+    CONSTRAINT pk_adobs PRIMARY KEY (nom) ,
+    CONSTRAINT fk_adobs FOREIGN KEY (nom_firma_comercial) REFERENCES firmes_comercials(nom) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
 CREATE TABLE estacions
 (
     nom CHAR(50),
-    PRIMARY KEY (nom)
-);
+    CONSTRAINT pk_estacions PRIMARY KEY (nom) 
+) ENGINE = InnoDB;
 
 CREATE TABLE plantes
 (
     nom_popular CHAR(50),
     nom_llati CHAR(50) UNIQUE NOT NULL,
     nom_estacio_floracio CHAR(50),
-    PRIMARY KEY (nom_popular),
-    FOREIGN KEY (nom_popular) REFERENCES estacions(nom) ON DELETE CASCADE ON UPDATE CASCADE
-);
+    CONSTRAINT pk_plantes PRIMARY KEY (nom_popular) ,
+    CONSTRAINT fk_estacions FOREIGN KEY (nom_estacio_floracio) REFERENCES estacions(nom) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
 CREATE TABLE dosis_adobs
 (
@@ -40,24 +40,69 @@ CREATE TABLE dosis_adobs
     nom_estacio CHAR(20),
     nom_adob CHAR(20),
     quantitat_adob FLOAT(5,2),
-    PRIMARY KEY (nom_planta, nom_estacio, nom_adob),
-    FOREIGN KEY (nom_planta) REFERENCES plantes(nom) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (nom_estacio) REFERENCES estacions(nom) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (nom_adob) REFERENCES adobs(nom) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT quantitat_adob CHECK (quantitat_adob >= 20 AND quantitat_adob <= 1000) NOT DEFERRABLE
-);
+    CONSTRAINT pk_dosis_adobs PRIMARY KEY (nom_planta, nom_estacio, nom_adob) ,
+    CONSTRAINT fk_dosis_adobs_to_plantes FOREIGN KEY (nom_planta) REFERENCES plantes(nom_popular) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_dosis_adobs_to_estacions FOREIGN KEY (nom_estacio) REFERENCES estacions(nom) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_dosis_adobs_to_adobs FOREIGN KEY (nom_adob) REFERENCES adobs(nom) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT quantitat_adob CHECK (quantitat_adob >= 20 AND quantitat_adob <= 1000) 
+) ENGINE = InnoDB;
 
 CREATE TABLE metodes_reproduccio
 (
-    nom_popular CHAR(50),
-    PRIMARY KEY
-);
+    nom CHAR(50),
+    CONSTRAINT pk_metodes_reproduccio PRIMARY KEY (nom)
+) ENGINE = InnoDB;
 
 CREATE TABLE reproduccions (
     nom_planta CHAR(50),
     nom_metode CHAR(50),
-    grau_exit SMALLINT,
-    PRIMARY KEY (nom_planta, nom_metode),
-    FOREIGN KEY (nom_planta) REFERENCES Plantes(nom_popular),
-    FOREIGN KEY (nom_metode) REFERENCES Metodes_Reproduccio(nom_popular)
-);
+    grau_exit CHAR(50),
+    CONSTRAINT pk_reproduccions PRIMARY KEY (nom_planta, nom_metode),
+    CONSTRAINT fk_reproduccions_to_plantes FOREIGN KEY (nom_planta) REFERENCES plantes(nom_popular) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_reproduccions_to_metodes_reproduccio FOREIGN KEY (nom_metode) REFERENCES metodes_reproduccio(nom) ON DELETE CASCADE ON UPDATE CASCADE 
+) ENGINE = InnoDB;
+
+CREATE TABLE plantes_interior
+(
+    nom_planta CHAR(50),
+    ubicacio_adient CHAR(50),
+    temperatura_adient FLOAT(5,2),
+    CONSTRAINT pk_plantes_interior PRIMARY KEY (nom_planta) 
+) ENGINE = InnoDB;
+
+CREATE TABLE plantes_exterior
+(
+    nom_planta CHAR(50),
+    cicle_de_vida SMALLINT,
+    CONSTRAINT pk_plantes_exterior PRIMARY KEY (nom_planta)
+) ENGINE = InnoDB;
+
+CREATE TABLE rec_plantes
+(
+    nom_planta_interior CHAR(50),
+    nom_estacio CHAR(50),
+    quantitat_aigua FLOAT(5,2),
+    CONSTRAINT pk_rec_plantes PRIMARY KEY (nom_planta_interior, nom_estacio) ,
+    CONSTRAINT fk_rec_plantes_to_plantes_interior FOREIGN KEY (nom_planta_interior) REFERENCES plantes_interior(nom_planta) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_rec_plantes_to_estacions FOREIGN KEY (nom_estacio) REFERENCES estacions(nom) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE paisos
+(
+    nom CHAR(50),
+    CONSTRAINT pk_paisos PRIMARY KEY (nom)
+) ENGINE = InnoDB;
+
+CREATE TABLE origen_plantes
+(
+    nom_planta_interior CHAR(50),
+    nom_pais CHAR(50),
+    CONSTRAINT pk_origen_plantes PRIMARY KEY (nom_planta_interior, nom_pais)
+) ENGINE = InnoDB;
+
+CREATE TABLE exemplars_plantes
+(
+    numero INT(2),
+    nom_planta CHAR(50),
+    CONSTRAINT pk_exemplars_plantes PRIMARY KEY (numero, nom_planta)
+) ENGINE = InnoDB;
--- insercions.sql
+++ insercions.sql
@@ -33,66 +33,66 @@ insert into plantes(nom_popular, nom_llati, nom_estacio_floracio) values ("Croto
 -- Exemplars plantes
 
 -- Firmes comercials
-insert into firmes_comercials(nom) values in ("URVADOB");
-insert into firmes_comercials(nom) values in ("TIRSADOB");
-insert into firmes_comercials(nom) values in ("PRISADOB");
-insert into firmes_comercials(nom) values in ("CIRSADOB");
+insert into firmes_comercials(nom) values ("URVADOB");
+insert into firmes_comercials(nom) values ("TIRSADOB");
+insert into firmes_comercials(nom) values ("PRISADOB");
+insert into firmes_comercials(nom) values ("CIRSADOB");
 
 -- Adobs
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Plantavit", "URVADOB", "LLD");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Vitaplant", "TIRSADOB", "AI");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Nutreplant", "CIRSADOB", "LLD");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Creixplant", "PRISADOB", "AI");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Casadob", "TIRSADOB", "AI");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Plantadob", "PRISADOB", "LLD");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Superplant", "CIRSADOB", "AI");
-insert into adobs(nom, tipus, nom_firma_comercial) values in ("Sanexplant", "URVADOB", "LLD");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Plantavit", "URVADOB", "LLD");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Vitaplant", "TIRSADOB", "AI");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Nutreplant", "CIRSADOB", "LLD");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Creixplant", "PRISADOB", "AI");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Casadob", "TIRSADOB", "AI");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Plantadob", "PRISADOB", "LLD");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Superplant", "CIRSADOB", "AI");
+insert into adobs(nom, nom_firma_comercial, tipus) values ("Sanexplant", "URVADOB", "LLD");
 
 -- Dosis d'adobs
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Gerani", "Primavera", "Casadob", 30.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Gerani", "Hivern", "Vitaplant", 20.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Begònia", "Estiu", "Casadob", 25.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Camèlia", "Hivern", "Plantavit", 50.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Camèlia", "Primavera", "Casadob", 75.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Ciclamen", "Tardor", "Casadob", 30.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Crisantem", "Primavera", "Casadob", 45.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Begònia", "Primavera", "Nutreplant", 50.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Roser", "Primavera", "Casadob", 50.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Roser", "Primavera", "Creixplant", 30.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Falguera", "Primavera", "Casadob", 40.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Falguera", "Tardor", "Plantadob", 20.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Tulipa", "Hivern", "Casadob", 40.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Potus", "Primavera", "Casadob", 30.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Cintes", "Tardor", "Casadob", 30.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Cintes", "Hivern", "Superplant", 40.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Poinsetia", "Hivern", "Casadob", 50.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Poinsetia", "Hivern", "Sanexplant", 45.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Heura", "Primavera", "Casadob", 50.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Croton", "Primavera", "Casadob", 40.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Croton", "Estiu", "Casadob", 60.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Gerani", "Estiu", "Sanexplant", 40.0);
-insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values in ("Ficus benjamina", "Primavera", "Casadob", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Gerani", "Primavera", "Casadob", 30.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Gerani", "Hivern", "Vitaplant", 20.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Begònia", "Estiu", "Casadob", 25.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Camèlia", "Hivern", "Plantavit", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Camèlia", "Primavera", "Casadob", 75.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Ciclamen", "Tardor", "Casadob", 30.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Crisantem", "Primavera", "Casadob", 45.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Begònia", "Primavera", "Nutreplant", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Roser", "Primavera", "Casadob", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Roser", "Primavera", "Creixplant", 30.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Falguera", "Primavera", "Casadob", 40.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Falguera", "Tardor", "Plantadob", 20.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Tulipa", "Hivern", "Casadob", 40.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Potus", "Primavera", "Casadob", 30.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Cintes", "Tardor", "Casadob", 30.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Cintes", "Hivern", "Superplant", 40.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Poinsetia", "Hivern", "Casadob", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Poinsetia", "Hivern", "Sanexplant", 45.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Heura", "Primavera", "Casadob", 50.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Croton", "Primavera", "Casadob", 40.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Croton", "Estiu", "Casadob", 60.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Gerani", "Estiu", "Sanexplant", 40.0);
+insert into dosis_adobs(nom_planta, nom_estacio, nom_adob, quantitat_adob) values ("Ficus benjamina", "Primavera", "Casadob", 50.0);
 
 -- Reproduccions
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Gerani", "Esqueix", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Begònia", "Esqueix", "Alt"); /* A l'enunciat posa Exqueix, error o posat a propòsit? */
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Begònia", "Capficats", "Alt"); 
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Begònia", "Llavors", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Roser", "Estques", "Mitjà");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Cintes", "Estolons", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Ciclamen", "Esqueix", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Ciclamen", "Capficats", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Potus", "Capficats", "Mitjà");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Potus", "Esqueix", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Tulipa", "Bulbs", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Ficus Benjamina", "Estaques", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Ficus Benjamina", "Capficats", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Croton", "Esqueix", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Croton", "Capficats", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Falguera", "Esqueix", "Mitjà");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Heura", "Esqueix", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Crisantem", "Bulbs", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Camèlia", "Estaques", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Heura", "Capficats", "Alt");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Poinsetia", "Llavors", "Baix");
-insert into reproduccions(nom_planta, nom_metode, grau_exit) insert into ("Poinsetia", "Esqueix", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Gerani", "Esqueix", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Begònia", "Esqueix", "Alt"); /* A l'enunciat posa Exqueix, error o posat a propòsit? */
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Begònia", "Capficats", "Alt"); 
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Begònia", "Llavors", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Roser", "Estaques", "Mitjà");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Cintes", "Estolons", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Ciclamen", "Esqueix", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Ciclamen", "Capficats", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Potus", "Capficats", "Mitjà");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Potus", "Esqueix", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Tulipa", "Bulbs", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Ficus Benjamina", "Estaques", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Ficus Benjamina", "Capficats", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Croton", "Esqueix", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Croton", "Capficats", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Falguera", "Esqueix", "Mitjà");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Heura", "Esqueix", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Crisantem", "Bulbs", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Camèlia", "Estaques", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Heura", "Capficats", "Alt");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Poinsetia", "Llavors", "Baix");
+insert into reproduccions(nom_planta, nom_metode, grau_exit) values ("Poinsetia", "Esqueix", "Baix");
