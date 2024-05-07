00d6dab8616b404e3986a9c0e8f7b636eeae4876
Ivan <imolimort@gmail.com>
2024-05-07 18:01:57 +0200

    update query 2

--- query_2.sql
+++ query_2.sql
@@ -6,7 +6,7 @@ AND P.nom_popular NOT IN (SELECT P_I.nom_planta
                           FROM plantes_interior P_I)
 AND P.nom_popular NOT IN (SELECT D.nom_planta
                           FROM dosis_adobs D
-                          WHERE D.nom_estacio = "Primavera")
+                          WHERE D.nom_estacio = "Primavera"
                           AND D.nom_adob IN (SELECT A.nom
                                              FROM adobs A
-                                             WHERE A.nom_firma_comercial = "TIRSADOB");
+                                             WHERE A.nom_firma_comercial = "TIRSADOB"));
