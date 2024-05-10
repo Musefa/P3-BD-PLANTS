27cfade7f3c37691bd2a5a38d018d4b3389588ea
Ivan <imolimort@gmail.com>
2024-05-10 15:52:58 +0200

    update query 3

--- query_3.sql
+++ query_3.sql
@@ -1,7 +1,7 @@
 -- Consulta número 3
-SELECT A.nom, A.tipus
+SELECT A.nom, A.tipus /* adobs - combinacions no existents -> atributs resultants */
 FROM adobs A
-WHERE A.nom NOT IN (SELECT A.nom
-                    FROM adobs A, dosis_adobs D
-                    WHERE (D.nom_planta, A.nom) NOT IN (SELECT D.nom_planta, D.nom_adob
-                                                        FROM dosis_adobs D));
+WHERE A.nom NOT IN (SELECT A.nom /* plantes x adobs - dosis_adobs -> combinacions no existents */
+                    FROM adobs A, plantes P
+                    WHERE (P.nom_popular, A.nom) NOT IN (SELECT D.nom_planta, D.nom_adob
+                                                         FROM dosis_adobs D));
