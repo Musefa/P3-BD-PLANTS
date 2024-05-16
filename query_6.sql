3015aa5a73856bd143102b62165e8e8de3cd4813
ivanmoliinero <imolimort@gmail.com>
2024-05-16 13:50:06 +0200

    first try and version of 6th query

---
+++ query_6.sql
@@ -0,0 +1,10 @@
+SELECT P_E.nom_planta, F.nom_estacio
+FROM plantes_exterior P_E LEFT OUTER JOIN floracio F ON (P_E.nom_planta = F.nom_planta)
+WHERE P_E.nom_planta IN (SELECT E.nom_planta 
+                         FROM exemplars_plantes E
+                         GROUP BY E.nom_planta
+                         HAVING COUNT(*) >= (SELECT max(count(*)
+                                             FROM exemplars_plantes E, plantes_interior P_I
+                                             WHERE P_I.nom_planta = E.nom_planta
+                                             GROUP BY P_I.nom_planta; /* Nombre d'exemplars de la planta d'interior amb més exemplars */)));
+
