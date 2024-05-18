5578c35700f9cbad1966c025a00fc5838884efa1
ivanmoliinero <imolimort@gmail.com>
2024-05-18 10:26:36 +0200

    query 6 update

--- query_6.sql
+++ query_6.sql
@@ -3,8 +3,8 @@ FROM plantes_exterior P_E LEFT OUTER JOIN floracio F ON (P_E.nom_planta = F.nom_
 WHERE P_E.nom_planta IN (SELECT E.nom_planta 
                          FROM exemplars_plantes E
                          GROUP BY E.nom_planta
-                         HAVING COUNT(*) >= (SELECT max(count(*)
-                                             FROM exemplars_plantes E, plantes_interior P_I
-                                             WHERE P_I.nom_planta = E.nom_planta
-                                             GROUP BY P_I.nom_planta; /* Nombre d'exemplars de la planta d'interior amb més exemplars */)));
+                         HAVING COUNT(*) >= ALL((SELECT COUNT(*) /* All perquè no es pot fer anar max correctament */
+                                                 FROM exemplars_plantes E, plantes_interior P_I
+                                                 WHERE P_I.nom_planta = E.nom_planta
+                                                 GROUP BY P_I.nom_planta))); /* Nombre d'exemplars de la planta d'interior amb més exemplars */
 
