887bd6f4aa2dac4f292bc9bef9dc3265f41d1c39
ivanmoliinero <imolimort@gmail.com>
2024-05-19 11:26:51 +0200

    update delete correction

--- delete.sql
+++ delete.sql
@@ -3,4 +3,6 @@ DELETE FROM adobs
 WHERE nom_firma_comercial = "URVADOB"
 AND nom NOT IN (SELECT nom_adob
                 FROM dosis_adobs
-                WHERE nom_estacio = "Hivern");
+                WHERE nom_planta IN (SELECT nom_planta
+                                     FROM floracio
+                                     WHERE nom_estacio = "Hivern"));
